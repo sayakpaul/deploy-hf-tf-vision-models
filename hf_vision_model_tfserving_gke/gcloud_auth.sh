8a44f414f1f3725c1d72c943eda70e6ca5e515cd
Chansung Park <deep.diver.csp@gmail.com>
2022-07-19 06:56:24 +0000

    t

--- hf_vision_model_tfserving_gke/gcloud_auth.sh
+++ hf_vision_model_tfserving_gke/gcloud_auth.sh
@@ -1,6 +1,5 @@
 export GCP_PROJECT_ID=gcp-ml-172005
-export SA_KEY_FILE=/tmp/gcp-ml-172005-5c6ccb3685b9.json
 
 eval "$(cat ~/.bashrc)"
-gcloud auth activate-service-account --key-file $SA_KEY_FILE
+gcloud auth login 
 gcloud config set project $GCP_PROJECT_ID
