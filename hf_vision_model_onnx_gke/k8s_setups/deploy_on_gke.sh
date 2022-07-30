d056f461659f3a44921441d94e2610167067dc78
Sayak Paul <spsayakpaul@gmail.com>
2022-07-30 20:36:50 +0530

    fix: deployment script.

--- hf_vision_model_onnx_gke/k8s_setups/deploy_on_gke.sh
+++ hf_vision_model_onnx_gke/k8s_setups/deploy_on_gke.sh
@@ -17,10 +17,9 @@ export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 gcloud container clusters get-credentials $GKE_CLUSTER_NAME --zone $GKE_CLUSTER_ZONE --project $GCP_PROJECT_ID  
 
 cd ../.kube
-export PATH=$PATH:$(pwd)
 
 # Deploy.
-kustomize build . | kubectl apply -f -
+./kustomize build . | kubectl apply -f -
 kubectl rollout status deployment/$GKE_DEPLOYMENT_NAME
 kubectl get services -o wide
 kubectl describe deployment $GKE_DEPLOYMENT_NAME
