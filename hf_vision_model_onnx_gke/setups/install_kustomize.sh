40b6840129872629dce7a4bd8e7e8521457db62c
Sayak Paul <spsayakpaul@gmail.com>
2022-07-30 20:00:09 +0530

    feat: add support for onnx deployment.
    Co-authored-by: Chansung Park <deep.diver.csp@gmail.com>

---
+++ hf_vision_model_onnx_gke/setups/install_kustomize.sh
@@ -0,0 +1,6 @@
+#! /bin/sh -i
+
+curl -sfLo kustomize.tar.gz https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.1.2/kustomize_v4.1.2_linux_amd64.tar.gz
+tar -zxvf kustomize.tar.gz
+rm kustomize.tar.gz
+chmod u+x ./kustomize
